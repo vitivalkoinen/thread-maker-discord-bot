4d8fc02da4c013ca3308694c0d73c2c4ec25db1a
vitivalkoinen <if.snow.else.neige@gmail.com>
2024-06-23 09:17:07 +0900

    update: send error message to discord webhook

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from zoneinfo import ZoneInfo
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import requests
 
 
 async def main():
@@ -27,7 +28,13 @@ class ThreadMakerBot(commands.Bot):
         print("Logged on as", self.user)
 
     async def on_error(self, event, *args, **kwargs):
-        print(f"Error in {event}: {args} {kwargs}")
+        webhook_url = "https://discord.com/api/webhooks/1254227830754250863/23XpJu3Up5ZlLmKv13PbeWMsGmbeUjfYgA3vyo_zBCVYdzg6jU6kVwUGt2efV0S8uXj1"
+        error_message = f"Error in {event}: {args} {kwargs}"
+        payload = {"content": error_message}
+
+        response = requests.post(webhook_url, json=payload)
+        if response.status_code != 204:
+            print(f"Failed to send error message to webhook: {response.status_code} {response.text}")
 
 
 class ThreadMakerCommand(commands.Cog):
