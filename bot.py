57a978da8693b0a9ab6d0c38c4c9aacf0df193ff
vitivalkoinen <if.snow.else.neige@gmail.com>
2024-10-26 00:26:23 +0900

    modified: ロジックの細分化

--- bot.py
+++ bot.py
@@ -34,7 +34,19 @@ class ThreadMakerBot(commands.Bot):
 
         response = requests.post(webhook_url, json=payload)
         if response.status_code != 204:
-            print(f"Failed to send error message to webhook: {response.status_code} {response.text}")
+            print(
+                f"Failed to send error message to webhook: {response.status_code} {response.text}"
+            )
+
+
+class ThreadMakerException(Exception):
+    def __init__(self, arg=""):
+        self.arg = arg
+
+
+class ThreadAlreadyExists(ThreadMakerException):
+    def __str__(self):
+        return f"this thread is already exists: {self.arg}"
 
 
 class ThreadMakerCommand(commands.Cog):
@@ -44,34 +56,54 @@ class ThreadMakerCommand(commands.Cog):
     @commands.command(name="make")
     async def make_thread(self, ctx: commands.Context):
         if not isinstance(ctx.channel, discord.TextChannel):
+            await ctx.send("テキストチャンネルではないのでスレッドを作成できません。")
             return
 
-        thread_name = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y/%m/%d の授業")
-
-        if self._is_exists_thread(ctx.channel, thread_name):
-            await ctx.send(f"スレッド「{thread_name}」は既に存在します。")
+        thread_name = self._generate_class_name()
+        try:
+            thread = await self._create_thread(ctx.channel, thread_name)
+            await self._add_all_members(thread, ctx.channel.members)
+        except ThreadAlreadyExists as e:
+            print(e)
+            await ctx.send("既に授業用スレッドが存在します")
             return
-
-        # 新しいスレッドを作成
-        thread = await ctx.message.create_thread(
-            name=thread_name, reason="授業用スレッドの作成"
-        )
+        except Exception as e:
+            raise e
 
         # そのチャンネルの全メンバーをスレッドに追加
-        for member in ctx.channel.members:
-            if not member.bot:
-                await thread.add_user(member)
 
         await ctx.send(
             f"スレッド「{thread.name}」が正常に作成され、全メンバーが追加されました。"
         )
 
-    def _is_exists_thread(
-        self, text_channel: discord.TextChannel, thread_name: str
-    ) -> bool:
+    def _is_exists_thread(self, txtch: discord.TextChannel, thread_name: str) -> bool:
         # 既存のスレッドをチェック
-        existing_threads = set(thread.name for thread in text_channel.threads)
+        existing_threads = set(thread.name for thread in txtch.threads)
         return thread_name in existing_threads
+
+    async def _create_thread(
+        self, txtch: discord.TextChannel, thread_name: str
+    ) -> discord.Thread:
+        if self._is_exists_thread(txtch, thread_name):
+            raise ThreadAlreadyExists(thread_name)
+
+        # 新しいスレッドを作成
+        thread = await txtch.create_thread(
+            name=thread_name, reason="授業用スレッドの作成"
+        )
+
+        return thread
+
+    async def _add_all_members(
+        self, thread: discord.Thread, members: list[discord.Member]
+    ) -> None:
+        for member in members:
+            if not member.bot:
+                await thread.add_user(member)
+
+    @staticmethod
+    def _generate_class_name() -> str:
+        return datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y/%m/%d の授業")
 
 
 if __name__ == "__main__":
