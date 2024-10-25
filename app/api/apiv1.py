6a2a71f8f8e967f985563cc720f793822e022cca
vitivalkoinen <if.snow.else.neige@gmail.com>
2024-10-26 03:17:03 +0900

    add: dummy api

--- app/api/apiv1.py
+++ app/api/apiv1.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Request
 from typing import Any
 
-from app.bot.threadmaker import ThreadMakerBot
+from discord.ext import commands
 
 
 class Index:
     router = APIRouter()
 
-    def __init__(self, bot: ThreadMakerBot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @router.get("/")
